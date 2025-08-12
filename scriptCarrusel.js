bc71cbc16b1190bcfe4376da3cb7f4635883a710
saacevedo <santur1989@gmail.com>
2025-08-12 15:14:39 -0500

    editando

---
+++ scriptCarrusel.js
@@ -0,0 +1,42 @@
+let slideIndex = 1;
+showSlides(slideIndex);
+
+function plusSlides(n) {
+    showSlides(slideIndex += n);
+}
+
+function currentSlide(n) {
+    showSlides(slideIndex = n);
+}
+
+function showSlides(n) {
+    let i;
+    let slides = document.getElementsByClassName("carousel-slide")[0].getElementsByTagName("img");
+    let dots = document.getElementsByClassName("dot");
+
+    if (n > slides.length) {
+        slideIndex = 1;
+    }
+    if (n < 1) {
+        slideIndex = slides.length;
+    }
+
+    for (i = 0; i < slides.length; i++) {
+        slides[i].style.display = "none";
+    }
+    
+    for (i = 0; i < dots.length; i++) {
+        dots[i].className = dots[i].className.replace(" active", "");
+    }
+    
+    
+    const slideImage = slides[slideIndex - 1];
+    const dotIndicator = dots[slideIndex - 1];
+    
+    if (slideImage) {
+        slideImage.style.display = "block";
+    }
+    if (dotIndicator) {
+        dotIndicator.className += " active";
+    }
+}
